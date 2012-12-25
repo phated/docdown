d1ea318e0bccefb4af33b746ed1b39bba219cc01
Blaine Bublitz <blaine@iceddev.com>
2012-12-24 21:10:21 -0700

    add parse.js to test against lodash

---
+++ parse.js
@@ -0,0 +1,19 @@
+var docdown = require("./docdown");
+
+var fs = require('fs');
+
+// generate Markdown
+var markdown = docdown({
+  path: '../lodash/lodash.js',
+  title: 'Lo-Dash <sup>v1.0.0-rc.3</sup>',
+  toc: 'categories',
+  url: 'https://github.com/bestiejs/lodash/blob/master/lodash.js'
+  // url: 'https://github.com/phated/docdown/blob/js/src/DocDown/Alias.js'
+});
+
+fs.writeFileSync('output.md', markdown, 'utf-8');
+
+    // 'path'  => '../' . $file,
+    // 'title' => 'Lo-Dash <sup>v1.0.0-rc.3</sup>',
+    // 'toc'   => 'categories',
+    // 'url'   => 'https://github.com/bestiejs/lodash/blob/master/lodash.js'
